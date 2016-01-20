83cefe3b1646ec06de6f64d9c574f301853a2162
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-20 10:32:50 +0100

    added notes list layouts

---
+++ app/src/main/java/pl/elpassion/window/sql_lite_note_app/NotesViewActivity.kt
@@ -0,0 +1,20 @@
+package pl.elpassion.window.sql_lite_note_app
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import java.util.*
+
+class NotesViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notes_list)
+
+    }
+}
