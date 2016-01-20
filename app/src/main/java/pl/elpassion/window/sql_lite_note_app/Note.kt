f48d99299381281170cbe67af936581c9a9c6f7a
jasiekpor <jasiekpor@gmail.com>
2016-01-20 10:37:42 +0100

    Note class

---
+++ app/src/main/java/pl/elpassion/window/sql_lite_note_app/Note.kt
@@ -0,0 +1,6 @@
+package pl.elpassion.window.sql_lite_note_app
+
+/**
+ * Created by jasiekpor on 20.01.2016.
+ */
+class Note(val title: String, val content: String)
--- app/src/main/java/pl/elpassion/window/sql_lite_note_app/NotesViewActivity.kt
+++ app/src/main/java/pl/elpassion/window/sql_lite_note_app/NotesViewActivity.kt
@@ -1,14 +1,7 @@
 package pl.elpassion.window.sql_lite_note_app
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
-import java.util.*
 
 class NotesViewActivity : AppCompatActivity() {
 
