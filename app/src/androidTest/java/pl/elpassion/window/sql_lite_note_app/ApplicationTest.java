8b102d39cf52e56a83cc8b2b9b8c0a922a86aafd
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-20 09:59:24 +0100

    init  commit

---
+++ app/src/androidTest/java/pl/elpassion/window/sql_lite_note_app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.elpassion.window.sql_lite_note_app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
