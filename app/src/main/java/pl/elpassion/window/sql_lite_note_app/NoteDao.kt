9d4583cd9002ed8cca11f522ac0d42e4651ee03b
jasiekpor <jasiekpor@gmail.com>
2016-01-20 13:08:57 +0100

    NoteDAO addOrUpdateNote

--- app/src/main/java/pl/elpassion/window/sql_lite_note_app/NoteDao.kt
+++ app/src/main/java/pl/elpassion/window/sql_lite_note_app/NoteDao.kt
@@ -1,8 +1,10 @@
 package pl.elpassion.window.sql_lite_note_app
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class NoteDAO(context: Context, name: String, factory: SQLiteDatabase.CursorFactory, version: Int) : SQLiteOpenHelper(context, name, factory, version) {
 
@@ -24,9 +26,48 @@ class NoteDAO(context: Context, name: String, factory: SQLiteDatabase.CursorFact
         val CREATE_NOTE_TABLE = " CREATE TABLE $TABLE_NOTE($KEY_NOTE_ID INTEGER PRIMARY KEY, $KEY_NOTE_TITLE TEXT, $KEY_NOTE_CONTENT TEXT)"
         db.execSQL(CREATE_NOTE_TABLE)
     }
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
-    public fun addOrUpdateNote(note : Note):Long{
 
-        return 1
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    public fun addOrUpdateNote(note: Note): Long {
+        val db = writableDatabase
+        var noteId: Long = -1
+
+        db.beginTransaction()
+        try {
+            val values = ContentValues()
+            values.put(KEY_NOTE_ID, note.id)
+            values.put(KEY_NOTE_TITLE, note.title)
+            values.put(KEY_NOTE_CONTENT, note.content)
+
+            val rows = db.update(TABLE_NOTE, values, KEY_NOTE_ID + "= ?", arrayOf(note.id.toString()))
+
+            if (rows == 1) {
+                val noteSelectQuery = "SELECT $KEY_NOTE_ID FROM $TABLE_NOTE WHERE $KEY_NOTE_ID = ?"
+                val cursor = db.rawQuery(noteSelectQuery, arrayOf(note.id.toString()))
+                try {
+                    if (cursor.moveToFirst()) {
+                        noteId = cursor.getLong(0);
+                        db.setTransactionSuccessful();
+                    }
+                } finally {
+                    if (cursor != null && !cursor.isClosed()) {
+                        cursor.close();
+                    }
+                }
+            } else {
+                noteId = db.insertOrThrow(TABLE_NOTE, null, values);
+                db.setTransactionSuccessful();
+            }
+            db.insertOrThrow(TABLE_NOTE, null, values)
+            db.setTransactionSuccessful()
+        } catch(e: Exception) {
+            Log.d("", "")
+        } finally {
+            db.endTransaction()
+        }
+
+        return noteId
     }
 }
