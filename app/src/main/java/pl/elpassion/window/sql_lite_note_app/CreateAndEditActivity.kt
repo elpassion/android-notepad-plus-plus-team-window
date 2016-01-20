042e1006f270fd40b53cd8f7d715716cd2c017b9
jasiekpor <jasiekpor@gmail.com>
2016-01-20 10:46:26 +0100

    menu create and edit view

--- app/src/main/java/pl/elpassion/window/sql_lite_note_app/CreateAndEditActivity.kt
+++ app/src/main/java/pl/elpassion/window/sql_lite_note_app/CreateAndEditActivity.kt
@@ -3,13 +3,22 @@ package pl.elpassion.window.sql_lite_note_app
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
 
 /**
  * Created by jasiekpor on 20.01.2016.
  */
+
+
 class CreateAndEditActivity : AppCompatActivity() {
+    private val title by lazy { findViewById(R.id.note_title) as EditText }
+    private val content by lazy { findViewById(R.id.note_content) as EditText }
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_notes)
+        setContentView(R.layout.create_and_edit_layout)
+    }
+    private fun saveChanges(){
+
     }
 }
