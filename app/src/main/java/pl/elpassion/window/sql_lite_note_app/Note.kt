3f4bbec13293a650c93cdc46d3409c06faffceb3
jasiekpor <jasiekpor@gmail.com>
2016-01-21 09:51:24 +0100

    Note Parcelable

--- app/src/main/java/pl/elpassion/window/sql_lite_note_app/Note.kt
+++ app/src/main/java/pl/elpassion/window/sql_lite_note_app/Note.kt
@@ -1,8 +1,24 @@
 package pl.elpassion.window.sql_lite_note_app
 
-import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
 /**
  * Created by jasiekpor on 20.01.2016.
  */
-class Note(val id: Int? = null, val title: String, val content: String):Serializable
+class Note(val id: Int? = null, val title: String, val content: String) : Parcelable {
+    companion object {
+        public fun createNoteFromParcel(parcel: Parcel): Note {
+            val data = parcel.createStringArray()
+            return Note(data[0].toInt(), data[1], data[2])
+        }
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeStringArray(arrayOf(this.id.toString(), title, content))
+    }
+
+    override fun describeContents(): Int {
+        throw UnsupportedOperationException()
+    }
+}
